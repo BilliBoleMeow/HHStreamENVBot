f23fe7e762b52db51306baf7b20bb48826c59b50
Parveen Bhadoo <87309724+PBhadoo@users.noreply.github.com>
2023-07-28 09:48:06 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -24,7 +24,7 @@ key = 'BHADOO9854752658' #16 char for AES128
 #FIX IV
 iv =  'CLOUD54158954721'.encode('utf-8') #16 char for AES128
 
-def decrypt(enc, key, iv):
+async def decrypt(enc, key, iv):
     enc = base64.b64decode(enc)
     cipher = AES.new(key.encode('utf-8'), AES.MODE_CBC, iv)
     decrypted = unpad(cipher.decrypt(enc), 16)
