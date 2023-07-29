efa5e5e66948fad3cbdf46bdd62f7baa613c3165
Parveen Bhadoo <87309724+PBhadoo@users.noreply.github.com>
2023-07-29 08:38:32 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -13,11 +13,5 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @StreamBot.on_message(filters.command(["start"]))
 async def start(_, m: Message):
     await m.reply(
-        f'Hello {m.from_user.mention(style="md")},\n\nSend me a file or You can just add me to any Telegram Channel and Use, to know more do /help.\n\nFollow @HashHackers for Support.'
-    )
-
-@StreamBot.on_message(filters.command(["help"]))
-async def start(_, m: Message):
-    await m.reply(
-        f'Hello {m.from_user.mention(style="md")},\n\nAdd me a file to get the domain link, replace your channel id with my own and message id of what you wanna download from your channel, but first add me to your channel with Admin Permissions.\n\nFollow @HashHackers for Support.'
+        f'Hello {m.from_user.mention(style="md")},\n\nYou are in the wrong place. Go to @LiquidXProjects or @HashHackers'
     )
